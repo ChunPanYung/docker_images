9aa8a77fddd39aaecc1bcd863140476ae09850f7
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2023-02-21 15:55:51 -0500

    Install ansible-core instead of ansible, install terraform.

--- code-server/init.sh
+++ code-server/init.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get upgrade --yes
-apt-get autremove --yes
-apt-get --yes install python3-pip
-pip install ansible-core ansible-lint yamllint
-pip install pre-commit
+apt update
+apt upgrade --yes
+apt autremove --yes
+apt --yes install python3-pip pipx
+apt install --yes gnupg software-properties-common
+
+# Dev tools
+pipx install pre-commit
+
+# Ansible tools
+pipx install ansible-core 
+pipx inject ansible-core --include-apps ansible-lint yamllint
+# terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt update && apt install --yes terraform
