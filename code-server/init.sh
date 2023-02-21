25d6c112d682ed7264db309e811206f21c5561f7
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2023-02-21 15:32:44 -0500

    Install ansible-core instead of ansible.

--- code-server/init.sh
+++ code-server/init.sh
@@ -3,5 +3,5 @@ apt-get update
 apt-get upgrade --yes
 apt-get autremove --yes
 apt-get --yes install python3-pip
-pip install ansible ansible-lint yamllint
+pip install ansible-core ansible-lint yamllint
 pip install pre-commit
